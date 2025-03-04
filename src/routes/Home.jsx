6296b056fda1280e297e5ad0409ed9eef4389c62
warling <warlinglopez01@gmail.com>
2025-03-04 11:49:12 -0400

    0.0.0P02

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 // Secciones
+import Inicio from "../sections/Inicio";
 import SeccionNosotros from "../sections/SeccionNosotros";
 import SeccionContacto from "../sections/SeccionContacto";
 
 const Home = () => {
   return (
     <>
+      <Inicio/>
       <SeccionNosotros />
       <SeccionContacto />
     </>
