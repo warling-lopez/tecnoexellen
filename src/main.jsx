e34886293a918abab580f0555d8802e75119bbba
TheyCallMeKev <kevin.amaurys@outlook.com>
2025-03-04 09:11:35 -0500

    arreglando lo que hizo warling

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './routes/Home.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <Router>
       <Routes>
         {/* Rutas principales */}
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
         
         {/* Ruta para manejar páginas no encontradas */}
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
-  </React.StrictMode>
+  </StrictMode>
 )
