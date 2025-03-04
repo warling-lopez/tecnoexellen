735ddbfe923ba14e2bc02b8e821c6807378e7ec0
warling <warlinglopez01@gmail.com>
2025-03-04 15:29:30 -0400

    creacion del la secion inicial

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,7 +5,7 @@ import SeccionContacto from "../sections/SeccionContacto";
 const Home = () => {
   return (
 
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center bg-gray-50">
       <Inicio/>
       <hr className="w-3/4 border-gray-300 my-4 mx-auto" />
       <SeccionNosotros />
