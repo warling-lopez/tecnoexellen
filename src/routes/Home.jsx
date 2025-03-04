478f8a3061465d546d551a620dd48f13325e67a8
TheyCallMeKev <kevin.amaurys@outlook.com>
2025-03-04 10:56:19 -0500

    hr del home arreglado

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,14 @@
-// Secciones
 import SeccionNosotros from "../sections/SeccionNosotros";
 import SeccionContacto from "../sections/SeccionContacto";
 
 const Home = () => {
   return (
-    <>
-      <hr className="w-3/4 border-gray-300 absolute right-[50%] translate-x-[50%]" />
+    <div className="flex flex-col items-center">
+      <hr className="w-3/4 border-gray-300 my-4 mx-auto" />
       <SeccionNosotros />
-      <hr className="w-3/4 border-gray-300 absolute right-[50%] translate-x-[50%]" />
+      <hr className="w-3/4 border-gray-300 my-4 mx-auto" />
       <SeccionContacto />
-    </>
+    </div>
   );
 };
 
