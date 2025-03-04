6cdae6e26998263a7e5b58557fd20636fe52b784
TheyCallMeKev <kevin.amaurys@outlook.com>
2025-03-04 10:34:09 -0500

    seccion hecha

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,10 +5,12 @@ import SeccionContacto from "../sections/SeccionContacto";
 const Home = () => {
   return (
     <>
+      <hr className="w-3/4 border-gray-300 absolute right-[50%] translate-x-[50%]" />
       <SeccionNosotros />
+      <hr className="w-3/4 border-gray-300 absolute right-[50%] translate-x-[50%]" />
       <SeccionContacto />
     </>
-  )
-}
+  );
+};
 
 export default Home;
--- src/sections/SeccionContacto.jsx
+++ src/sections/SeccionContacto.jsx
@@ -1,7 +1,25 @@
+import ServiceCard from "../components/ServiceCard";
+
 const SeccionContacto = () => {
   return (
-    <h1>h</h1>
-  )
-}
+    <div>
+      <h2 className="font-semibold text-2xl">¿Te interesan nuestros servicios?</h2>
+      {/* Service list */}
+      <div className="flex flex-col gap-5 p-3">
+        <ServiceCard />
+        <ServiceCard />
+        <ServiceCard />
+      </div>
+      <div className="flex flex-col items-center gap-4">
+        <span className="text-center font-semibold text-xl">
+          ¡Contactanos ya!
+        </span>
+        <button type="button" className="bg-blue-900 text-white font-semibold py-2 w-2/4 rounded hover:-translate-y-0.5 hover:bg-blue-950 transition cursor-pointer">
+          Clíck aquí
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default SeccionContacto;
