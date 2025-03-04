12306541e1094d9af62f878fcaf54869cbe64320
TheyCallMeKev <kevin.amaurys@outlook.com>
2025-03-04 10:35:27 -0500

    merge con rama de kevin, seccion completada

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,12 @@
+const ServiceCard = () => {
+  return (
+    <div className="bg-blue-950 rounded-lg p-4 text-white">
+      <span className="font-semibold text-gray-300">NombreDelServicio</span>
+      <p className="pl-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus commodi consectetur corrupti sed rerum molestias perspiciatis ex architecto, ducimus amet quos quis, exercitationem tenetur delectus eaque consequuntur, quas optio libero.
+      Pariatur nisi amet quidem earum dolorum possimus ipsum magni fuga impedit? Officia ut voluptatum voluptates exercitationem aliquam aliquid tempore nesciunt. Et minus debitis officia magnam esse laudantium nulla expedita quidem.
+      Obcaecati laboriosam, labore libero porro vero reiciendis maiores, ipsum, aliquam necessitatibus deserunt hic nisi itaque est odit. Quo, fuga. Quas nulla veniam quos voluptates harum ad eveniet cumque minima earum.</p>
+    </div>
+  )
+}
+
+export default ServiceCard;
