9a9f9b111d6698113feac2d59c401bd5d543ffda
Karm Stu. <monterokarm@gmail.com>
2025-03-05 16:04:04 -0400

    Filosofia agregada

--- src/sections/SeccionNosotros.jsx
+++ src/sections/SeccionNosotros.jsx
@@ -7,16 +7,41 @@ const SeccionNosotros = () => {
         <h2 className="text-gray-800 text-2xl font-bold">Sobre Nosotros</h2>
         <hr />
         <div className="bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition">
-          <h2 className="font-bold text-gray-700 text-xl">¿Qué es Technoexellen?</h2>
+          <h2 className="font-bold text-gray-700 text-xl">Misión</h2>
           <p className="pl-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, molestias mollitia. Suscipit odit voluptatum corporis ea repellat quas minima atque asperiores, labore voluptate consequatur doloribus eos at et ipsa velit.
+            Brindar soluciones tecnológicas innovadoras y personalizadas en
+            automatización, conectividad y seguridad para hogares y empresas.
+            Nos enfocamos en ofrecer productos y servicios de alta calidad,
+            garantizando confianza, eficiencia y bienestar a nuestros clientes a
+            través de la tecnología
           </p>
         </div>
         <div className="bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition">
-          <h2 className="font-bold text-gray-700 text-xl">¿Por qué deberia elegir Technoexellen?</h2>
+          <h2 className="font-bold text-gray-700 text-xl">Visión</h2>
           <p className="pl-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas eius quos, quo voluptas aliquid placeat repudiandae et nisi quaerat magnam cupiditate. Explicabo ex, saepe laudantium minus neque iste ut harum?
+            Ser la empresa líder en soluciones tecnológicas integrales,
+            reconocida por su compromiso con la innovación, la excelencia en el
+            servicio y la satisfacción del cliente. Aspiramos a transformar la
+            manera en que las personas y las empresas interactúan con la
+            tecnología, mejorando su calidad de vida y productividad.
           </p>
+        </div>
+        <div className="bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition">
+          <h2 className="font-bold text-gray-700 text-xl">Valores</h2>
+          <ul className="pl-5">
+            <li>
+              Compromiso: Nos enfocamos en brindar un servicio confiable y
+              adaptado a las necesidades de cada cliente.
+            </li>
+            <li>
+              Seguridad: Protegemos los espacios de nuestros clientes con
+              tecnología confiable y eficiente.
+            </li>
+            <li>
+              Responsabilidad: Actuamos con ética y transparencia en cada
+              proyecto que desarrollamos.
+            </li>
+          </ul>
         </div>
       </div>
       <hr className="border-l h-96 min-md:block hidden" />
