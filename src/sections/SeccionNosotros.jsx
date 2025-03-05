403d480f7fc6416b220baf603ca52be32ed10693
Karm Stu. <monterokarm@gmail.com>
2025-03-05 11:06:08 -0400

    se agrego hover en la seccion de nosotros dentro del texto

--- src/sections/SeccionNosotros.jsx
+++ src/sections/SeccionNosotros.jsx
@@ -2,31 +2,35 @@ import StarRating from "../components/StarRating";
 
 const SeccionNosotros = () => {
   return (
-    <div className="flex max-md:flex-col justify-around p-2">
+    <div className="flex max-md:flex-col justify-around p-2 bg-white shadow-lg">
       <div className="p-3 gap-3 flex flex-1 flex-col">
         <h2 className="text-gray-800 text-2xl font-bold">Sobre Nosotros</h2>
         <hr />
-        <h2 className="font-bold text-gray-700 text-xl">
-          ¿Qué es Technoexcellen?
-        </h2>
-        <p className="pl-5">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-          at, quos atque eos quas reprehenderit earum, laudantium, quibusdam
-          aspernatur totam provident! Recusandae ipsum minima alias quo optio
-          dolore, ratione unde!
-        </p>
-        <h2 className="font-bold text-gray-700 text-xl">
-          ¿Por qué elegir Technoexellen?
-        </h2>
-        <p className="pl-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis illum
-          quaerat earum quibusdam dolore officia accusamus amet et sapiente
-          repudiandae deleniti, doloremque exercitationem. Odio quae dolorem
-          assumenda quisquam quos consectetur!
-        </p>
+        <div className="bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition">
+          <h2 className="font-bold text-gray-700 text-xl">
+            ¿Qué es Technoexcellen?
+          </h2>
+          <p className="pl-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            at, quos atque eos quas reprehenderit earum, laudantium, quibusdam
+            aspernatur totam provident! Recusandae ipsum minima alias quo optio
+            dolore, ratione unde!
+          </p>
+        </div>
+        <div className="bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition">
+          <h2 className="font-bold text-gray-700 text-xl">
+            ¿Por qué elegir Technoexellen?
+          </h2>
+          <p className="pl-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis illum
+            quaerat earum quibusdam dolore officia accusamus amet et sapiente
+            repudiandae deleniti, doloremque exercitationem. Odio quae dolorem
+            assumenda quisquam quos consectetur!
+          </p>
+        </div>
       </div>
       <hr className="border-l h-96 min-md:block hidden" />
-      <div className="bg-white flex flex-1 flex-col rounded-xl p-2 items-center text-black gap-2">
+      <div className="flex flex-1 flex-col rounded-xl p-2 items-center text-black gap-2">
         <img
           className="rounded-full w-[200px] h-[200px] shadow-lg"
           src="/OIP.jpeg"
@@ -34,7 +38,7 @@ const SeccionNosotros = () => {
         />
         <h2 className="text-2xl text-gray-500">Juansito</h2>
         <p className="p-2 rounded-xl text-gray-900 text-center">
-            Excelente Servicio, ¡me encanto!
+          Excelente Servicio, ¡me encanto!
         </p>
         <StarRating />
       </div>
