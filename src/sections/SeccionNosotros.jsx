0f612efa8d6a46a0f1e94e862c038c8577dd9c0a
Karm Stu. <monterokarm@gmail.com>
2025-03-06 10:54:40 -0400

    ,

--- src/sections/SeccionNosotros.jsx
+++ src/sections/SeccionNosotros.jsx
@@ -7,27 +7,26 @@ const SeccionNosotros = () => {
         <div className="bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition">
           <h2 className="font-bold text-gray-700 text-xl">Misión</h2>
           <p className="pl-5">
-            Brindar soluciones tecnológicas innovadoras y personalizadas en
-            automatización, conectividad y seguridad para hogares y empresas.
-            Nos enfocamos en ofrecer productos y servicios de alta calidad,
-            garantizando confianza, eficiencia y bienestar a nuestros clientes a
-            través de la tecnología.
+            Brindar soluciones tecnologicas en la automatización, conectividad y
+            seguridad para hogares y empresas. Nos enfocamos en ofrecer
+            productos y servicios de alta calidad, garantizando confianza,
+            seguridad y bienestar a nuestros clientes a través de la
+            tecnología.
           </p>
         </div>
         <div className="bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition">
           <h2 className="font-bold text-gray-700 text-xl">Visión</h2>
           <p className="pl-5">
-            Ser la empresa líder en soluciones tecnológicas integrales,
-            reconocida por su compromiso con la innovación, la excelencia en el
+            Ser la empresa líder en servicios y soluciones tecnológicas
+            integrales, reconocida por su compromiso con la excelencia en el
             servicio y la satisfacción del cliente. Aspiramos a transformar la
             manera en que las personas y las empresas interactúan con la
             tecnología, mejorando su calidad de vida y productividad.
           </p>
         </div>
-        <div className="bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition"></div>
       </div>
       <hr className="border-l h-96 min-md:block hidden" />
-      <div className="flex flex-1 flex-col rounded-xl p-2 items-center text-black gap-2 ">
+      <div className="flex flex-1 flex-col rounded-xl p-2 items-center text-black gap-2">
         <h2 className="font-bold text-gray-700 text-xl">Valores</h2>
         <ul className="pl-6 list-disc bg-white rounded-lg py-2 px-5 hover:shadow-xl hover:bg-zinc-100 transition">
           <li>
@@ -36,7 +35,7 @@ const SeccionNosotros = () => {
           </li>
           <li>
             Seguridad: Protegemos los espacios de nuestros clientes con
-            tecnología confiable y eficiente.
+            tecnología confiable y segura.
           </li>
           <li>
             Responsabilidad: Actuamos con ética y transparencia en cada proyecto
